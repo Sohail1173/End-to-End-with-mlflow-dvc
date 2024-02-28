2d906dac8893394c44f8a1ef838e3dafa0aeb159
Sohail1173 <mdsohailahmed711@gmail.com>
2024-02-28 11:34:39 +0530

    artifacts added

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from src.cancerClassification import logger
-import logging
+from src.cancerClassification.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
+STAGE_NAME="Data  Ingestion Stage"
 
-
-logging.info("welcome to cancerclassification")
+try:
+        logger.info(f">>>>>>>>>>>>>>stage{STAGE_NAME} started <<<<<<")
+        obj=DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>>>>>>>>>>>> stage{STAGE_NAME} completed <<<<<<\n\n============x")
+except Exception as e:
+        logger.exception(e)
+        raise e
--- src/cancerClassification/utils/common.py
+++ src/cancerClassification/utils/common.py
@@ -1,7 +1,7 @@
 import os
 from box.exceptions import BoxValueError
 import yaml
-from cancerClassification import logger
+from src.cancerClassification import logger
 import json
 import joblib
 from ensure import ensure_annotations
